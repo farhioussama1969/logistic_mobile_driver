0fd7c01edda8af224f3b24e7b9427d467482a230
Farhi oussama baha eddine <farhioussama1969@gmail.com>
2024-08-11 15:56:35 -0700

    fixes

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -51,7 +51,7 @@ class DefaultFirebaseOptions {
 
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyAAQPxNpNiWxdJIg318-H3a4bUZtf0gZlw',
-    appId: '1:427296167611:android:b2965ab09ffa46adbfbb9e',
+    appId: '1:427296167611:android:a921f8bf13a0b729bfbb9e',
     messagingSenderId: '427296167611',
     projectId: 'loogisti-38a9e',
     storageBucket: 'loogisti-38a9e.appspot.com',
